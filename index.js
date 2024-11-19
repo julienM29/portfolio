242cd9aff20ac3fec884963b1a724f40181e4377
julienM29 <miossec.julien@orange.fr>
2024-11-19 19:12:32 +0100

    Optimisation performances 1

--- index.js
+++ index.js
@@ -62,16 +62,6 @@ gsap.from(".project-item", {
     }
   });
 
-  // Animation des boutons au survol
-  gsap.utils.toArray(".project-item a").forEach(btn => {
-    btn.addEventListener("mouseenter", () => {
-      gsap.to(btn, { scale: 1.1, duration: 0.3, ease: "power2.out" });
-    });
-    btn.addEventListener("mouseleave", () => {
-      gsap.to(btn, { scale: 1, duration: 0.3, ease: "power2.in" });
-    });
-  });
-
   // Animation pour la partie contact
 gsap.to(".fade-in", {
     scrollTrigger: {
@@ -85,3 +75,23 @@ gsap.to(".fade-in", {
     ease: "power1.out",
   });
   
+  document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    var form = event.target;
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert('Votre message a été envoyé avec succès !');
+            window.location.reload();
+        } else {
+            alert('Une erreur est survenue lors de l\'envoi du message. Veuillez réessayer.');
+        }
+    }).catch(error => {
+        alert('Erreur : ' + error.message);
+    });
+});
--- projet.js
+++ projet.js
@@ -1,3 +1,22 @@
+// Barre de navigation afin d'enlever l'url bizarre
+document.addEventListener('DOMContentLoaded', function () {
+    const link = document.querySelector('a[href^="../index.html#"]');
+
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href = 'index.html';
+                setTimeout(() => {
+            const targetId = this.getAttribute('href').split('#')[1]; 
+            const targetElement = document.getElementById(targetId); 
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+            history.pushState(null, null, window.location.pathname);
+        }, 100); 
+    });
+});
+
 let isMobile = window.matchMedia("(max-width: 768px)").matches;
 gsap.registerPlugin(ScrollTrigger);
 
